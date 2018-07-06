d0184098f5fefa78e2ea12c7378b3b8f3cc30b82
Joey <kuang7156@gmail.com>
2018-07-06 10:11:16 +0800

    modify comment

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name} Pattabhi team welcome to Docker Container..!</h3>" \
+    html = "<h3>Hello {name} Joey ! Have a nice day ! Welcome to Docker Container..!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "SoftGen"), hostname=socket.gethostname(), visits=visits)
